258359281f58b3c72895cd850ff71577a4589f5b
noeli <noeli@172.17.116.101>
2025-03-28 11:48:25 +0100

    premier commit

---
+++ src/main/java/com/mycompany/projet_infom2/Projet_InfoM2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_infom2;
+
+/**
+ *
+ * @author noeli
+ */
+public class Projet_InfoM2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
