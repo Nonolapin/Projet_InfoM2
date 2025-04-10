428ac9fc4373bee78ad256f2aceab2c3b2ae13b4
noeli <noeli@172.17.116.101>
2025-04-10 11:17:07 +0200

    version 10/04 avec petit test inclus (inclus gamme)

--- src/main/java/com/mycompany/projet_infom2/Projet_InfoM2.java
+++ src/main/java/com/mycompany/projet_infom2/Projet_InfoM2.java
@@ -12,17 +12,13 @@ public class Projet_InfoM2 {
 
     public static void main(String[] args) {
        
-        
-        
-       
-        
-        Machine m1 = new Machine ("machine1" , "mac_decoupe", 2.234f, 3.777f);
+        Machine m1 = new Machine ("machine1" , "mac_decoupe", 100, 2, 2.234f, 3.777f, 2);
         Machine.ajoutermachineliste (m1); 
-        Machine m2 = new Machine ("machine2" , "mac_assemblage", 3.99f, 5.23f);
+        Machine m2 = new Machine ("machine2" , "mac_assemblage",200, 3, 3.99f, 5.23f, 4);
         Machine.ajoutermachineliste (m2); 
         Machine.afficherToutesLesMachines(); 
         
-        Poste poste1 = new Poste ("poste1", "poste_telephone") ; 
+        Poste poste1 = new Poste ("poste1", "poste_telephone", 3, 300) ; 
         // Ajouter des machines aux postes
         poste1.getMachines().add(m1);  
         poste1.getMachines().add(m2);  
@@ -36,6 +32,7 @@ public class Projet_InfoM2 {
         Produit.ajouterProduit_stock (p2); 
         Produit.afficherStock (); 
         
+        Operation ope1 = new Operation ("operation001", "decoupe" , 3f, 400); 
         
         Operateur o1 = new Operateur ("Jean", "op001");
         // On occupe la personne
@@ -51,7 +48,19 @@ public class Projet_InfoM2 {
         }
         
         
-        Equipement e1 = new Equipement ("eq001", 3);
+        /*Equipement e1 = new Equipement ("eq001", 3);
+        Equipement e2 = new Equipement ("eq002", 2);
+        Operation ope1 = new Operation ("ope001", "couper", 3f); 
+        Operation ope2 = new Operation ("ope002", "fermer", 4f); */
+        
+        Gamme g1 = new Gamme ("g200") ; 
+        g1.creerGamme_eq(poste1); // Ajout d'un poste
+        g1.creerGamme_eq(m2); //Ajout d'une machine
+        g1.creerGamme_op(ope1) ; //Ajout d'une operation
+        Gamme.afficherGamme (g1); 
+        
+        Gamme.coutGamme(g1); 
+        Gamme.dureeGamme (g1);
         
         
         
